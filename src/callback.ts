b3cb091c8324199b8a6a4eb90692e1f89b236f1f
CENSOR_1337 <viixxivvimm@gmail.com>
2023-07-09 22:18:18 +0700

    refactor: `Callback` is now extends from shared `Callback`

--- src/callback.ts
+++ src/callback.ts
@@ -1,25 +1,23 @@
 import { randomUUID } from "./uuid";
 import { Event } from "./event";
 import { CFXEventData } from "@fivemjs/shared";
+import { Callback as CallbackShared } from "@fivemjs/shared";
 
-function triggerServerCallback<T>(eventName: string, ...args: any[]): Promise<T> {
-	const cbId = randomUUID();
-	const promise = new Promise<T>((resolve, reject) => {
-		Event.onServer(cbId, (data: any) => {
-			resolve(data as T);
+export class Callback extends CallbackShared {
+	public static emit<T>(eventName: string, ...args: any[]): Promise<T> {
+		const cbId = randomUUID();
+		const promise = new Promise<T>((resolve, reject) => {
+			Event.onServer(cbId, (data: any) => {
+				resolve(data as T);
+			});
 		});
-	});
-	Event.emitServer(`cslib:svcb:${eventName}`, cbId, ...args);
-	return promise;
-}
+		Event.emitServer(`${this.serverNamespace}:${eventName}`, cbId, ...args);
+		return promise;
+	}
 
-function registerClientCallback(eventName: string, handler: (...args: any[]) => void): CFXEventData {
-	return Event.onServer(`cslib:clcb:${eventName}`, (cbId: string, ...args: any[]) => {
-		Event.emitServer(cbId, handler(...args));
-	});
-}
-
-export class Callback {
-	public static emit = triggerServerCallback;
-	public static register = registerClientCallback;
+	public static register(eventName: string, handler: (...args: any[]) => void): CFXEventData {
+		return Event.onServer(`${this.clientNamespace}:${eventName}`, (cbId: string, ...args: any[]) => {
+			Event.emitServer(cbId, handler(...args));
+		});
+	}
 }
