57653dbae0930027e87c5652dafce3fb6e902056
CENSOR_1337 <viixxivvimm@gmail.com>
2023-07-09 21:51:45 +0700

    fix: emitServer require no target

--- src/resource.ts
+++ src/resource.ts
@@ -2,7 +2,7 @@ import { Resource as ResourceShared } from "@fivemjs/shared";
 import { Event } from "./event";
 
 export class Resource extends ResourceShared {
-	public static emitServer(eventName: string, target: number, ...args: any[]) {
-		return Event.emitServer(this.getEventName(eventName), target, ...args);
+	public static emitServer(eventName: string, ...args: any[]) {
+		return Event.emitServer(this.getEventName(eventName), ...args);
 	}
 }
