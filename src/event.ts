78f27800ada81800352ca225ae29c7e331f75255
CENSOR_1337 <viixxivvimm@gmail.com>
2023-07-09 22:26:15 +0700

    chore: create type for listener

--- src/event.ts
+++ src/event.ts
@@ -1,17 +1,19 @@
 import { Event as EventShared, Cfx } from "@fivemjs/shared";
 import { CFXEventData } from "@fivemjs/shared";
 
+export type listener = (...args: any[]) => void;
+
 export class Event extends EventShared {
 	public static emitServer(eventName: string, ...args: any[]): void {
 		return Cfx.triggerServerEvent(eventName, ...args);
 	}
- 
-	public static onServer(eventName: string, listener: (...args: any[]) => void): CFXEventData {
+
+	public static onServer(eventName: string, listener: listener): CFXEventData {
 		Cfx.addNetEventListener(eventName, listener);
 		return { eventName, listener } as CFXEventData;
 	}
 
-	public static onceServer(eventName: string, listener: (...args: any[]) => void): CFXEventData {
+	public static onceServer(eventName: string, listener: listener): CFXEventData {
 		let eventData: CFXEventData;
 		eventData = this.onServer(eventName, (...args: any[]) => {
 			listener(...args);
