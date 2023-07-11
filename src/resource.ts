da2328b132d7bb8ab3d4311927ab39cd0fdc0459
CENSOR_1337 <viixxivvimm@gmail.com>
2023-07-11 11:15:31 +0700

    feat: add `Callback` to `Resource`

--- src/resource.ts
+++ src/resource.ts
@@ -1,8 +1,30 @@
 import { Resource as ResourceShared } from "@fivemjs/shared";
 import { Event } from "./event";
+import { Callback } from "./callback";
+import { CFXEventData } from "@fivemjs/shared";
+
+class ResourceCallback extends Callback {
+	public static emit<T>(eventName: string, ...args: any[]): Promise<T> {
+		return super.emit(Resource.getEventName(eventName), ...args);
+	}
+
+	public static register(eventName: string, handler: (...args: any[]) => void): CFXEventData {
+		return super.register(Resource.getEventName(eventName), handler);
+	}
+}
 
 export class Resource extends ResourceShared {
+	public static readonly Callback = ResourceCallback;
+    
 	public static emitServer(eventName: string, ...args: any[]) {
 		return Event.emitServer(this.getEventName(eventName), ...args);
+	}
+
+	public static onServer(eventName: string, handler: (...args: any[]) => void): CFXEventData {
+		return Event.onServer(this.getEventName(eventName), handler);
+	}
+
+	public static onceServer(eventName: string, handler: (...args: any[]) => void): CFXEventData {
+		return Event.onceServer(this.getEventName(eventName), handler);
 	}
 }
