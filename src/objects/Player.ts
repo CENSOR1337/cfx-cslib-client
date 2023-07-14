6e9a6f4bb7779e68f7203aa45a23eb233b31a0cf
CENSOR_1337 <viixxivvimm@gmail.com>
2023-07-14 22:38:57 +0700

    feat(Player):
     * added fromPedhandle function
    
     * added from fromServerId function

--- src/objects/Player.ts
+++ src/objects/Player.ts
@@ -3,7 +3,7 @@ import { Vector3 } from "@fivemjs/shared";
 export class Player {
 	public static player: Player;
 
-	constructor(public readonly playerId: number) {}
+	constructor(public readonly handle: number) {}
 
 	public static get local(): Player {
 		if (!Player.player) {
@@ -12,8 +12,29 @@ export class Player {
 		return Player.player;
 	}
 
+	public static fromPedHandle(ped: number): Player {
+		return new Player(NetworkGetPlayerIndexFromPed(ped));
+	}
+
+	public static fromServerId(serverId: number): Player {
+		return new Player(GetPlayerFromServerId(serverId));
+	}
+
+	public static get all(): Player[] {
+		const players: Player[] = [];
+		const activePlayers = GetActivePlayers();
+		for (let i = 0; i < activePlayers.length; i++) {
+			players.push(new Player(activePlayers[i]));
+		}
+		return players;
+	}
+
+	public get isDead(): boolean {
+		return IsPlayerDead(this.handle);
+	}
+
 	public get ped(): number {
-		return GetPlayerPed(this.playerId);
+		return GetPlayerPed(this.handle);
 	}
 
 	public get pos(): Vector3 {
